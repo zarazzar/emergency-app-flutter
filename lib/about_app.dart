de514cf11ec18d1a36654552a444c82844415033
zaraazzar <azzar.arza18@gmail.com>
2023-06-04 02:28:08 +0700

    add data polisi

--- lib/about_app.dart
+++ lib/about_app.dart
@@ -18,23 +18,6 @@ class AboutApp extends StatelessWidget {
   }
 }
 
-class Heading extends StatelessWidget {
-  final String text;
-
-  const Heading({Key? key, required this.text}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: const TextStyle(
-        fontSize: 24.0,
-        fontWeight: FontWeight.bold,
-      ),
-    );
-  }
-}
-
 class nextDesc extends StatefulWidget {
   final String text;
   const nextDesc({Key? key, required this.text}) : super(key: key);
